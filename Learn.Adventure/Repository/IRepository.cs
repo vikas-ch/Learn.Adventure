8278c9a96e015a3c564d34093ae0f06ad68ee8c5
Vikas C <vikas.ch@gmail.com>
2022-02-22 19:27:07 +0530

    initial structure (2)

---
+++ Learn.Adventure/Repository/IRepository.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Learn.Adventure.Models.Abstractions;
+
+namespace Learn.Adventure.Repository
+{
+    public interface IRepository<T> where T : IDocument
+    {
+        IQueryable<T> AsQueryable();
+
+        IEnumerable<T> FilterBy(
+            Expression<Func<T, bool>> filterExpression);
+
+        IEnumerable<TProjected> FilterBy<TProjected>(
+            Expression<Func<T, bool>> filterExpression,
+            Expression<Func<T, TProjected>> projectionExpression);
+
+        T FindOne(Expression<Func<T, bool>> filterExpression);
+
+        Task<T> FindOneAsync(Expression<Func<T, bool>> filterExpression);
+
+        T FindById(string id);
+
+        Task<T> FindByIdAsync(string id);
+        
+        void InsertOne(T document);
+
+        Task InsertOneAsync(T document);
+    }
+}
